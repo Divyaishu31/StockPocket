6a7db3b340d51ba6b79f62effe96ee80b7fd05a1
mohit-kakkar94 <77057852+mohit-kakkar94@users.noreply.github.com>
2021-04-20 18:05:20 +0530

    Update __init__.py
    Renamed app to application

--- project/__init__.py
+++ project/__init__.py
@@ -5,35 +5,35 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 
 #Set up Flask application
-app = Flask(__name__)
+application = Flask(__name__)
 
 #Set secret key for forms
-app.config["SECRET_KEY"] = "mysecret"
+application.config["SECRET_KEY"] = "mysecret"
 
 ##################################################
                 #Set up database#
 ##################################################
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "data.sqlite")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+application.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "data.sqlite")
+application.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db = SQLAlchemy(app)
-Migrate(app, db)
+db = SQLAlchemy(application)
+Migrate(application, db)
 
 ##################################################
                 #Login configurations#
 ##################################################
 loginManager = LoginManager()
 
-loginManager.init_app(app)
+loginManager.init_app(application)
 
 loginManager.login_view = "users.login"
 
 #Register blueprint in project/core/views.py
 from project.core.views import core
-app.register_blueprint(core)
+application.register_blueprint(core)
 
 
 #Register blueprint in project/users/views.py
 from project.users.views import users
-app.register_blueprint(users)
+application.register_blueprint(users)
