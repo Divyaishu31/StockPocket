583c2563fca82a8df38384958d03cc5ac6e9e540
divyaishu <divyaishu@pop-os.localdomain>
2021-05-01 02:42:35 +0530

    test cases updated

--- tests/test_models.py
+++ tests/test_models.py
@@ -24,37 +24,3 @@ def test_portfolio(portfolio):
 def test_project():
     response = application.test_client().get('/')
     assert response.status_code == 200
-
-
-
-
-
-
-
-
-
-
-
-# def test_empty_db(client):
-#     """Start with a blank database."""
-#
-#     rv = client.get('/')
-#     assert b'No entries here so far' in rv.data
-#
-# import unittest
-# from project import application
-# BasicTestCase(unittest.TestCase):
-#     def test_home(self):
-#         tester = app.test_client(self)
-#         response = tester.get('/', content_type='html/text')
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(response.data, b'Hello World!')def test_other(self):
-#         tester = app.test_client(self)
-#         response = tester.get('a', content_type='html/text')
-#         self.assertEqual(response.status_code, 404)
-#         self.assertTrue(b'does not exist' in response.data)if __name__ == '__main__':
-#     unittest.main()
-
-def test_application(client):
-    pass
-    #assert not client.application.debug
